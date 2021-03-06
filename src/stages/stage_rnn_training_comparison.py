952744c803d4356307ba2d545fa400293da46bf7
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2021-03-06 12:49:03 -0600

    another typo

--- src/stages/stage_rnn_training_comparison.py
+++ src/stages/stage_rnn_training_comparison.py
@@ -47,7 +47,7 @@ class RnnTrainingComparisonStage(BaseStage):
                                                                             lstm_config))
             train_valid_losses = np.genfromtxt(file_path, delimiter=", ", skip_header=1)
             train_losses = train_valid_losses[:][0]
-            valid_losses = train_valid_losses[:][0]
+            valid_losses = train_valid_losses[:][1]
 
             plt.plot(valid_losses, label="{} validation perplexity".format(lstm_config))
             plt.plot(train_losses, label="{} training perplexity".format(lstm_config))
