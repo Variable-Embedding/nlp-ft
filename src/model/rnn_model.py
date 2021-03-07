018c837a6ce960ce8f03726bbff382138f4eb283
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2021-03-06 20:54:25 -0600

    Add dropout and increase number of layers

--- src/model/rnn_model.py
+++ src/model/rnn_model.py
@@ -229,11 +229,15 @@ class LSTM(nn.Module):
         if self.configuration != 0:
             self.ff = nn.Sequential(
                 nn.Linear((1 + 2 * number_of_layers) * embedding_size, 2 * embedding_size),
-                nn.Tanh(), #nn.Dropout(dropout_probability),
+                nn.Tanh(), nn.Dropout(dropout_probability),
                 nn.Linear(2 * embedding_size, 2 * embedding_size),
-                nn.Tanh(), #nn.Dropout(dropout_probability),
+                nn.Tanh(), nn.Dropout(dropout_probability),
                 nn.Linear(2 * embedding_size, 2 * embedding_size),
-                nn.Tanh(), #nn.Dropout(dropout_probability),
+                nn.Tanh(), nn.Dropout(dropout_probability),
+                nn.Linear(2 * embedding_size, 2 * embedding_size),
+                nn.Tanh(), nn.Dropout(dropout_probability),
+                nn.Linear(2 * embedding_size, 2 * embedding_size),
+                nn.Tanh(), nn.Dropout(dropout_probability),
                 nn.Linear(2 * embedding_size, embedding_size),
                 nn.Tanh()
             )
