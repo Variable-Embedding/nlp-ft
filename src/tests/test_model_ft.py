ddc4b98098b2a84858d4d17f8414f59072ab1151
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-04-22 13:04:43 -0400

    Update test_model_ft.py

--- src/tests/test_model_ft.py
+++ src/tests/test_model_ft.py
@@ -9,13 +9,3 @@ class TestModel(TestCase):
     def test_forward(self):
         corpus_type = 'wikitext2'
         embedding_type = 'glove.6B.50d'
-
-        benchmark = GetBenchmarkCorpra(corpus_type=corpus_type)
-        benchmark.run()
-        print(len(benchmark.corpra[2]))
-
-        pre_trained_embedding = GetPreTrainedEmbeddingsStage(embedding_type=embedding_type)
-        pre_trained_embedding.run()
-
-        # data = Benchmark2Embeddings(embedding_type=embedding_type, corpus_type=corpus_type)
-        # data.run()
